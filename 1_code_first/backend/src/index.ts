1b2a8d8767dabb9a504e0087d0dd82c71b3be223
Kevin Do <do.kevin.vo@gmail.com>
2026-03-12 00:44:56 -0700

    feat: create user and get user list with hono js

--- 1_code_first/backend/src/index.ts
+++ 1_code_first/backend/src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { eq } from "drizzle-orm";
 import { usersTable } from "./db/schema";
+import { Hono } from "hono";
 
 import { config } from "dotenv";
 
@@ -11,27 +12,55 @@ console.log("url: ", process.env.DATABASE_URL);
 
 const db = drizzle(process.env.DATABASE_URL!);
 
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  };
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
+const app = new Hono();
 
+app.get("/users", async (context) => {
   const users = await db.select().from(usersTable);
 
-  console.log("Getting all users from the database: ", users);
+  return context.json({
+    ok: true,
+    data: users,
+  });
+});
 
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log("User deleted!");
-}
-main();
+app.post("/users", async (context) => {
+  const body = await context.req.parseBody();
+
+  const user: typeof usersTable.$inferInsert = {
+    name: body["name"] as string,
+    age: Number(body["age"]),
+    email: body["email"] as string,
+  };
+
+  const doesEmailExist = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.email, user.email))
+    .limit(1);
+
+  if (doesEmailExist.length) {
+    return context.json(
+      {
+        error: "EmailAlreadyInUse",
+        data: null,
+        success: false,
+      },
+      409
+    );
+  }
+
+  await db.insert(usersTable).values(user);
+
+  const result = await db.select().from(usersTable).where(eq(usersTable, user));
+
+  return context.json(
+    {
+      error: null,
+      data: result,
+      success: true,
+    },
+    201
+  );
+});
+
+export default app;
